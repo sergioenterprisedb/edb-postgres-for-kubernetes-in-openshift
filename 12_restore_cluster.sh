79e56009cf5b14e1d54ca51ea0571ed546b5ce97
Sergio Romera <sergio.romera@enterprisedb.com>
2023-10-13 12:58:34 +0200

    Bug fixed in printf line

--- 12_restore_cluster.sh
+++ 12_restore_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./config.sh
-printf "${green}echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql cluster-sample${reset}\n"
+printf "${green}echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql cluster-sample -- -U postgres${reset}\n"
 printf "${green}${kubectl_cmd} apply -f ./yaml/restore.yaml${reset}\n"
 
 echo "select pg_switch_wal()" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
