f1d2e0df791e5c91defa71daa772cbc578e7c4ce
Sergio Romera <sergio.romera@enterprisedb.com>
2024-01-18 16:38:35 +0100

    bug fixed

--- 13_failover.sh
+++ 13_failover.sh
@@ -7,5 +7,5 @@ printf "${yellow}Deleting pvc and pod from primary instance ${primary}...${reset
 
 #kubectl delete pvc/cluster-example-2 pod/cluster-example-2 --force
 printf "${green}${kubectl_cmd} delete pvc/${primary} pod/${primary} --force${reset}\n"
-${kubectl_cmd} delete pvc/${primary} pod/${primary} --force
+#${kubectl_cmd} delete pvc/${primary} pod/${primary} --force
 ${kubectl_cmd} delete pvc/${primary} pvc/${primary}-wal pod/${primary} --force
