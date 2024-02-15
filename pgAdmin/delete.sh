0372dee0fcfde9fa0662d237776c713e889ec78b
Sergio Romera <sergio.romera@enterprisedb.com>
2024-02-15 11:13:51 +0100

    pgAdmin in OpenShift

--- pgAdmin/delete.sh
+++ pgAdmin/delete.sh
@@ -5,3 +5,7 @@ kubectl delete -f pgadmin-deployment.yaml
 kubectl delete -f pgadmin-service.yaml
 kubectl delete -f route.yaml
 
+kubectl delete -f security-context.yaml
+kubectl delete sa sa-pga
+kubectl delete -f rbca.yaml
+
--- pgAdmin/deploy.sh
+++ pgAdmin/deploy.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-kubectl apply -f pgadmin-configmap.yaml
-kubectl apply -f pgadmin-deployment.yaml
-kubectl apply -f pgadmin-service.yaml
-kubectl apply -f route.yaml
+# 
+oc adm policy add-scc-to-group anyuid system:serviceaccounts:openshift-operators
+
+# Create custom security context constraints
+oc apply -f security-context.yaml
+# Create a new service account for the deployment, use the following command
+oc create sa sa-pga
+# Setup a role and role binding to use SCC and service account. Please use the following resource yaml.
+oc apply -f rbca.yaml
+
+
+oc apply -f pgadmin-configmap.yaml
+oc apply -f pgadmin-deployment.yaml
+oc apply -f pgadmin-service.yaml
+oc apply -f route.yaml
 
 echo "**********************************************************************"
 echo "Connect to: http://pgadmin-client-openshift-operators.apps-crc.testing"
 echo "User: test@test.com"
 echo "Password: test"
+echo "Database password: `./password_app.sh`"
 echo "**********************************************************************"
 
