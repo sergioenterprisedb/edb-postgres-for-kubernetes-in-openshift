6d8be285bcd59b30baeb6bf22804bbf85557f730
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 17:45:26 +0100

    Demo env update

--- 12_restore_cluster.sh
+++ 12_restore_cluster.sh
@@ -3,7 +3,7 @@
 print_command "echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql cluster-sample -- -U postgres\n"
 print_command "${kubectl_cmd} apply -f ./yaml/restore.yaml\n"
 
-print_command "select pg_switch_wal()" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
+print_command "select pg_switch_wal()\n" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
 sleep 3
 envsubst < ./yaml/restore.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
 
