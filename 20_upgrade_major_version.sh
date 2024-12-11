b011b2942c00239a18bc15133ed41c7478f49669
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 18:39:12 +0100

    Demo env update

--- 20_upgrade_major_version.sh
+++ 20_upgrade_major_version.sh
@@ -2,6 +2,6 @@
 
 . ./config.sh
 
-print_command "${kubectl_cmd} apply -f cluster-sample-upgrade-16-to-17.yaml\n"
+print_command "${kubectl_cmd} apply -f ${cluster_name}-upgrade-16-to-17.yaml\n"
 
 envsubst < ./yaml/cluster-sample-upgrade-16-to-17.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
