0dea0d21cdd9635c7409f69c89cbd78742e05839
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 19:02:06 +0100

    Demo env update

--- 60_pgbouncer.sh
+++ 60_pgbouncer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . ./config.sh
 
-${kubectl_cmd} apply -f ./yaml/pgbouncer.yaml
+#${kubectl_cmd} apply -f ./yaml/pgbouncer.yaml
+envsubst < ./yaml/pgbouncer.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
 sleep 5
 
--- 61_pgbouncer_port_forward.sh
+++ 61_pgbouncer_port_forward.sh
@@ -2,8 +2,8 @@
 
 . ./config.sh
 
-printf "${green}${kubectl_cmd} port-forward service/pooler-sample-rw  6432:5432 &${reset}\n"
-${kubectl_cmd} port-forward service/pooler-sample-rw  6432:5432 &
+print_command "${kubectl_cmd} port-forward service/pooler-${cluster_name}-rw  6432:5432 &\n"
+${kubectl_cmd} port-forward service/pooler-${cluster_name}-rw  6432:5432 &
 pid1=$!
-printf "${green}Port forward process ID: ${red}${pid1}${reset}\n"
+print_info "Port forward process ID: ${red}${pid1}\n"
 
