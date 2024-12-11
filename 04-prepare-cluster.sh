0ad65f82c98bcff0d7c63ac964cd8e782605eb13
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 19:17:03 +0100

    Demo env update

--- 04-prepare-cluster.sh
+++ 04-prepare-cluster.sh
@@ -2,8 +2,8 @@
 
 . ./config.sh
 
-#cp yaml/cluster-sample-upgrade-${object_storage_type}.yaml yaml/cluster-sample-upgrade.yaml
-#envsubst < yaml/cluster-sample-upgrade-${object_storage_type}.yaml >yaml/cluster-sample-upgrade.yaml
+# Create namespace if does not exists
+. ./create_namespace.sh
 
 if [ "$object_storage_type" == "aws" ]; then
   print_info "=======================================================\n"
@@ -16,9 +16,6 @@ if [ "$object_storage_type" == "aws" ]; then
 elif [ "$object_storage_type" == "minio" ]; then
   print_info "Minio secrets not yet configured...\n"
 fi
-
-# Create namespace if does not exists
-. ./create_namespace.sh
 
 # Set context
 ./set_context.sh
