51d822ec9f17b1f09828f4e2a9194bedce6c5429
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 18:01:16 +0100

    Demo env update

--- 17_pitr_backup.sh
+++ 17_pitr_backup.sh
@@ -4,4 +4,5 @@ print_command "${kubectl_cmd} delete -f ./yaml/backup.yaml\n"
 ${kubectl_cmd} delete -f ./yaml/backup.yaml
 sleep 3
 print_command "${kubectl_cmd} apply -f ./yaml/backup.yaml\n"
-${kubectl_cmd} apply -f ./yaml/backup.yaml
+envsubst < ./yaml/backup.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
+
