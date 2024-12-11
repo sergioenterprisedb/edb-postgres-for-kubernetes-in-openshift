64d24a5c5300bf277f68ffa5eaa984a5e3c3717e
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 17:35:24 +0100

    Demo env update

--- 10_backup_cluster.sh
+++ 10_backup_cluster.sh
@@ -4,7 +4,8 @@ print_command "${kubectl_cmd} delete -f ./yaml/backup.yaml\n"
 ${kubectl_cmd} delete -f ./yaml/backup.yaml
 
 printf "${kubectl_cmd} apply -f ./yaml/backup.yaml\n"
-${kubectl_cmd} apply -f ./yaml/backup.yaml
+#${kubectl_cmd} apply -f ./yaml/backup.yaml
+envsubst < ./yaml/backup.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
 
 printf "echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql cluster-sample -- -U postgres\n"
 echo "select pg_switch_wal()" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
