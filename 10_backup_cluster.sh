0831fedf6daeb5abd35dba69fd46815f29ad9414
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-11 17:43:02 +0100

    Demo env update

--- 10_backup_cluster.sh
+++ 10_backup_cluster.sh
@@ -3,10 +3,9 @@
 print_command "${kubectl_cmd} delete -f ./yaml/backup.yaml\n"
 ${kubectl_cmd} delete -f ./yaml/backup.yaml
 
-printf "${kubectl_cmd} apply -f ./yaml/backup.yaml\n"
-#${kubectl_cmd} apply -f ./yaml/backup.yaml
+print_command "${kubectl_cmd} apply -f ./yaml/backup.yaml\n"
 envsubst < ./yaml/backup.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
 
-printf "echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql cluster-sample -- -U postgres\n"
+print_command "echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres\n"
 echo "select pg_switch_wal()" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
 
--- 12_restore_cluster.sh
+++ 12_restore_cluster.sh
@@ -3,8 +3,7 @@
 print_command "echo \"select pg_switch_wal()\" | ${kubectl_cnp} psql cluster-sample -- -U postgres\n"
 print_command "${kubectl_cmd} apply -f ./yaml/restore.yaml\n"
 
-echo "select pg_switch_wal()" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
+print_command "select pg_switch_wal()" | ${kubectl_cnp} psql ${cluster_name} -- -U postgres
 sleep 3
-#${kubectl_cmd} apply -f ./yaml/restore.yaml
 envsubst < ./yaml/restore.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
 
