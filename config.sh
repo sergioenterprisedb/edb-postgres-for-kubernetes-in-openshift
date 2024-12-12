f3fa5dac457647b4ab11485c80ebaa23d5d93a5d
Sergio Romera <sergio.romera@enterprisedb.com>
2024-12-12 15:33:54 +0100

    update

--- config.sh
+++ config.sh
@@ -7,11 +7,15 @@ export kubectl_cmd="oc"
 export kubectl_cnp="kubectl-cnp"
 
 # Template: edb-<region>-<your-first-name>
-# Ex:       edb-emea-sergio
-export namespace="edb-emea-<your_name>"
+# Ex:       edb-emea-user1
+export namespace="edb-<region>-<name>"
 
 # Postgres
-export cluster_name="cluster-<your_name>"
+
+# Templace: "cluster-<name>"
+# Ex: .     cluster-user1
+export cluster_name="cluster-<name>"
+
 export postgres_instances=3
 export postgres_cpu="0.5"
 export postgres_memory="512Mi"
@@ -19,14 +23,15 @@ export postgres_storage="512Mi"
 export postgres_wal_storage="512Mi"
 export postgres_default_image="quay.io/enterprisedb/postgresql:16.1"
 export postgres_upgrade_image="quay.io/enterprisedb/postgresql:16.4"
+export postgres_17_image="quay.io/enterprisedb/postgresql:17"
 
 # S3
 # --
-# Object storage environment [aws|minio]
+# Object storage environment [aws]
 export object_storage_type="aws"
-export object_storage_bucket="<your_bucket>"
+export object_storage_bucket="sergio1-bucket"
 # aws
-export s3_destination_path="s3://<your_bucket>/"
+export s3_destination_path="s3://sergio1-bucket/"
 export s3_endpoint_url=""
 # Minio
 #export s3_destination_path="s3://database-backups/"
