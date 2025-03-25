8cadcf88d2792cb8c4fde8c2904aa9586ea13df8
Sergio Romera <sergio.romera@enterprisedb.com>
2025-03-25 16:49:40 +0100

    update config.sh

--- aws_secrets.sh
+++ aws_secrets.sh
@@ -4,6 +4,19 @@
 
 kubectl delete secret minio-creds -n ${namespace}
 
+if [[ ! -f ".aws.sh" ]]; then
+  echo ""
+  echo "Error: File .aws.sh not found."
+  echo ""
+  echo "Create .aws.sh file with this information:"
+  echo ""
+  echo "export AWS_ACCESS_KEY_ID=\"<AWS_ACCESS_KEY_ID>\""
+  echo "export AWS_SECRET_ACCESS_KEY=\"<AWS_SECRET_ACCESS_KEY>\""
+  echo "export AWS_SESSION_TOKEN=\"<AWS_SESSION_TOKEN>\""
+  echo ""
+  exit 1
+fi
+
 . ./.aws.sh
 
 kubectl create secret generic minio-creds \
--- config.sh
+++ config.sh
@@ -2,20 +2,18 @@
 
 . ./commands.sh
 
+# Variables to be replaced
+export id="<your-name>"                  # your name or id
+export region="<your-region>"            # emea,na,apj
+export namespace="edb-${region}-${id}"   # k8s namespaceA
+export bucket="<your-bucket>"            # S3 bucket name
+
 # Kubernetes environment
 export kubectl_cmd="oc"
 export kubectl_cnp="kubectl-cnp"
 
-# Template: edb-<region>-<your-first-name>
-# Ex:       edb-emea-user1
-export namespace="edb-<region>-<name>"
-
 # Postgres
-
-# Templace: "cluster-<name>"
-# Ex: .     cluster-user1
-export cluster_name="cluster-<name>"
-
+export cluster_name="cluster-${id}"
 export postgres_instances=3
 export postgres_cpu="0.5"
 export postgres_memory="512Mi"
@@ -25,17 +23,23 @@ export postgres_default_image="quay.io/enterprisedb/postgresql:16.1"
 export postgres_upgrade_image="quay.io/enterprisedb/postgresql:16.4"
 export postgres_17_image="quay.io/enterprisedb/postgresql:17"
 
+# EPAS
+export epas_image="docker.enterprisedb.com/k8s_enterprise/edb-postgres-advanced:17.2"
+export epas_storage="512Mi"
+
 # S3
-# --
 # Object storage environment [aws]
 export object_storage_type="aws"
-export object_storage_bucket="sergio1-bucket"
+export object_storage_bucket="${bucket}"
+
 # aws
-export s3_destination_path="s3://sergio1-bucket/"
+export s3_destination_path="s3://${bucket}/"
 export s3_endpoint_url=""
+
 # Minio
 #export s3_destination_path="s3://database-backups/"
 #export s3_endpoint_url="http://minio-client-minio-operator.apps-crc.testing"
+
 # Minio local
 #export s3_destination_path="s3://database-backups/"
 #s3_endpoint_url="http://172.17.0.2:9000"
