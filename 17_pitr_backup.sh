47b1b9357989136a38a5c47fb28b0dadfb7b29f3
Sergio Romera <sergio.romera@enterprisedb.com>
2025-05-23 16:01:16 +0200

    Backup name fixed

--- 17_pitr_backup.sh
+++ 17_pitr_backup.sh
@@ -4,9 +4,9 @@
 #Doc
 echo "17" > ./docs/docid
 
-print_command "${kubectl_cmd} delete -f ./yaml/backup.yaml\n"
-${kubectl_cmd} delete -f ./yaml/backup.yaml
+print_command "${kubectl_cmd} delete backup ${cluster_name}-backup-test\n"
+${kubectl_cmd} delete backup ${cluster_name}-backup-test
 sleep 3
+
 print_command "${kubectl_cmd} apply -f ./yaml/backup.yaml\n"
 envsubst < ./yaml/backup.yaml | ${kubectl_cmd} apply -n ${namespace} -f-
-
