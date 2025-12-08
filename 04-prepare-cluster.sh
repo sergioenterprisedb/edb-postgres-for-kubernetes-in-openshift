47b7eb41c3fbd7056a6bb8d26cb7da9929ec0708
Sergio Romera <sergio.romera@enterprisedb.com>
2025-12-08 16:03:03 +0100

    Update

--- 04-prepare-cluster.sh
+++ 04-prepare-cluster.sh
@@ -17,7 +17,13 @@ if [ "$object_storage_type" == "aws" ]; then
   print_info "=======================================================\n"
   . ./aws_secrets.sh
 elif [ "$object_storage_type" == "minio" ]; then
-  print_info "Minio secrets configured...\n"
+  print_info "Minio secrets configuration...\n"
+  
+  print_command "${kubectl_cmd} delete secret minio-creds"
+  print_command "${kubectl_cmd} create secret generic minio-creds \\
+    --from-literal=ACCESS_KEY_ID=${ACCESS_KEY_ID} \\
+    --from-literal=ACCESS_SECRET_KEY=${ACCESS_SECRET_KEY}\n"
+
   ${kubectl_cmd} delete secret minio-creds
   ${kubectl_cmd} create secret generic minio-creds \
     --from-literal=ACCESS_KEY_ID=${ACCESS_KEY_ID} \
