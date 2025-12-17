d1f2f2b952a5f89bb542648cd15ec954ec6361b6
Sergio Romera <sergio.romera@enterprisedb.com>
2025-12-17 15:12:25 +0100

    Ensuring access to EDB private registry

--- delete_all_clusters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl delete cluster cluster-example-13
-kubectl delete cluster cluster-example-14
-kubectl delete cluster cluster-sample
-kubectl delete cluster cluster-sample-15
-
-
